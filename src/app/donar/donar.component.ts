81146c1486841de676e42648e68914a6842a36a6
Roshna Maruti Toke <54335159+roshna1924@users.noreply.github.com>
2020-05-14 17:09:32 -0500

    testing

--- src/app/donar/donar.component.ts
+++ src/app/donar/donar.component.ts
@@ -43,7 +43,7 @@ export class DonarComponent implements OnInit {
   openDialog(): void {
 
     const dialogRef = this.dialog.open(DialogComponent, {
-      width : '800px',
+      width : '600px',
       data : {username: this.username},
     });
 
